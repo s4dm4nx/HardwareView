7395849b277be744bb23c2fda98573c71a48c106
S4D M4N <119116750+s4dm4nx@users.noreply.github.com>
2022-11-26 20:42:24 +0300

    Add files via upload

---
+++ HardwareView/Main.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace HardwareView
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.CPUTemp = new System.Windows.Forms.Label();
+            this.GPUTemp = new System.Windows.Forms.Label();
+            this.Updater = new System.Windows.Forms.Timer(this.components);
+            this.Background = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CPUTemp
+            // 
+            this.CPUTemp.AutoSize = true;
+            this.CPUTemp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.CPUTemp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CPUTemp.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CPUTemp.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.CPUTemp.Location = new System.Drawing.Point(67, 20);
+            this.CPUTemp.Name = "CPUTemp";
+            this.CPUTemp.Size = new System.Drawing.Size(34, 23);
+            this.CPUTemp.TabIndex = 1;
+            this.CPUTemp.Text = "0 C";
+            // 
+            // GPUTemp
+            // 
+            this.GPUTemp.AutoSize = true;
+            this.GPUTemp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.GPUTemp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.GPUTemp.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.GPUTemp.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.GPUTemp.Location = new System.Drawing.Point(67, 53);
+            this.GPUTemp.Name = "GPUTemp";
+            this.GPUTemp.Size = new System.Drawing.Size(34, 23);
+            this.GPUTemp.TabIndex = 2;
+            this.GPUTemp.Text = "0 C";
+            // 
+            // Updater
+            // 
+            this.Updater.Interval = 1000;
+            this.Updater.Tick += new System.EventHandler(this.Updater_Tick);
+            // 
+            // Background
+            // 
+            this.Background.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.Background.ContextMenuStrip = this.contextMenuStrip1;
+            this.Background.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Background.Location = new System.Drawing.Point(0, 0);
+            this.Background.Name = "Background";
+            this.Background.Size = new System.Drawing.Size(120, 96);
+            this.Background.TabIndex = 0;
+            this.Background.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.closeToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(104, 26);
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.closeToolStripMenuItem.Text = "Close";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Location = new System.Drawing.Point(17, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 23);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "CPU";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label2.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.Location = new System.Drawing.Point(17, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "GPU";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 96);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.GPUTemp);
+            this.Controls.Add(this.CPUTemp);
+            this.Controls.Add(this.Background);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Opacity = 0.9D;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.TransparencyKey = System.Drawing.Color.FromArgb(((int)(((byte)(12)))), ((int)(((byte)(12)))), ((int)(((byte)(12)))));
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Background)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Background;
+        private System.Windows.Forms.Label CPUTemp;
+        private System.Windows.Forms.Label GPUTemp;
+        private System.Windows.Forms.Timer Updater;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ HardwareView/Main.cs
@@ -0,0 +1,224 @@
+﻿using LibreHardwareMonitor.Hardware;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace HardwareView
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+            AddMouseMoveHandler(this);
+        }
+
+        static float cpuTemp;
+        // CPU Usage
+        static float cpuUsage;
+        // CPU Power Draw (Package)
+        static float cpuPowerDrawPackage;
+        // CPU Frequency
+        static float cpuFrequency;
+        // GPU Temperature
+        static float gpuTemp;
+        // GPU Usage
+        static float gpuUsage;
+        // GPU Core Frequency
+        static float gpuCoreFrequency;
+        // GPU Memory Frequency
+        static float gpuMemoryFrequency;
+
+ 
+
+        static Computer c = new Computer()
+        {
+            IsGpuEnabled = true,
+            IsCpuEnabled = true,
+            //RAMEnabled = true, // uncomment for RAM reports
+            //MainboardEnabled = true, // uncomment for Motherboard reports
+            //FanControllerEnabled = true, // uncomment for FAN Reports
+            //HDDEnabled = true, // uncomment for HDD Report
+        };
+        static String NameFile = "location.txt";
+        private void Main_Load(object sender, EventArgs e)
+        {
+            if (File.Exists(NameFile))
+            {
+                try
+                {
+                    string location = File.ReadAllText(NameFile);
+                    string posX = location.Split(' ')[0];
+                    string posY = location.Split(' ')[1];
+                    this.Location = new Point(Convert.ToInt32(posX), Convert.ToInt32(posY));
+                }
+                catch { }
+            }
+
+            new Thread(new ThreadStart(UpdateData)).Start();
+            c.Open();
+            Updater.Start();
+
+
+        }
+
+        private void Updater_Tick(object sender, EventArgs e)
+        {
+
+            CPUTemp.Text = cpuTemp.ToString() + " C°";
+            GPUTemp.Text = gpuTemp.ToString() + " C°";
+
+        }
+        static void UpdateData()
+        {
+            while (true)
+            {
+                try
+                {
+                    foreach (var hardware in c.Hardware)
+                    {
+
+                        if (hardware.HardwareType == HardwareType.Cpu)
+                        {
+                            // only fire the update when found
+                            hardware.Update();
+
+                            // loop through the data
+                            foreach (var sensor in hardware.Sensors)
+                                if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("CPU Package"))
+                                {
+                                    // store
+                                    cpuTemp = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("cpuTemp: " + sensor.Value.GetValueOrDefault());
+
+                                }
+                                else if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("CPU Total"))
+                                {
+                                    // store
+                                    cpuUsage = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("cpuUsage: " + sensor.Value.GetValueOrDefault());
+
+                                }
+                                else if (sensor.SensorType == SensorType.Power && sensor.Name.Contains("CPU Package"))
+                                {
+                                    // store
+                                    cpuPowerDrawPackage = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("CPU Power Draw - Package: " + sensor.Value.GetValueOrDefault());
+
+
+                                }
+                                else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("CPU Core #1"))
+                                {
+                                    // store
+                                    cpuFrequency = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("cpuFrequency: " + sensor.Value.GetValueOrDefault());
+                                }
+                        }
+
+
+                        // Targets AMD & Nvidia GPUS
+                        if (hardware.HardwareType == HardwareType.GpuAmd || hardware.HardwareType == HardwareType.GpuNvidia)
+                        {
+                            // only fire the update when found
+                            hardware.Update();
+
+                            // loop through the data
+                            foreach (var sensor in hardware.Sensors)
+                                if (sensor.SensorType == SensorType.Temperature && sensor.Name.Contains("GPU Core"))
+                                {
+                                    // store
+                                    gpuTemp = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("gpuTemp: " + sensor.Value.GetValueOrDefault());
+                                }
+                                else if (sensor.SensorType == SensorType.Load && sensor.Name.Contains("GPU Core"))
+                                {
+                                    // store
+                                    gpuUsage = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("gpuUsage: " + sensor.Value.GetValueOrDefault());
+                                }
+                                else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("GPU Core"))
+                                {
+                                    // store
+                                    gpuCoreFrequency = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("gpuCoreFrequency: " + sensor.Value.GetValueOrDefault());
+                                }
+                                else if (sensor.SensorType == SensorType.Clock && sensor.Name.Contains("GPU Memory"))
+                                {
+                                    // store
+                                    gpuMemoryFrequency = sensor.Value.GetValueOrDefault();
+                                    // print to console
+                                    System.Diagnostics.Debug.WriteLine("gpuMemoryFrequency: " + sensor.Value.GetValueOrDefault());
+                                }
+
+                        }
+
+                        // ... you can access any other system information you want here
+
+                    }
+                    Thread.Sleep(1000);
+                }
+                catch { }
+            }
+        }
+
+        private void AddMouseMoveHandler(Control c)
+        {
+            c.MouseMove += MouseMoveHandler;
+            c.ContextMenuStrip = contextMenuStrip1;
+            if (c.Controls.Count > 0)
+            {
+                foreach (Control ct in c.Controls)
+                    AddMouseMoveHandler(ct);
+            }
+        }
+
+        public static void WriteFile(string text)
+        {
+            if (File.Exists(NameFile))
+            {
+                File.Delete(NameFile);
+            }
+            File.WriteAllText(NameFile, text);
+        }
+
+        private void MouseMoveHandler(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
+                new Thread(() => WriteFile(getLocation())).Start();
+            }
+        }
+        string getLocation()
+        {
+            return this.Location.X + " " + this.Location.Y;
+        }
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HT_CAPTION = 0x2;
+
+        [DllImportAttribute("user32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        [DllImportAttribute("user32.dll")]
+        public static extern bool ReleaseCapture();
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+ 
+    }
+
+    
+
+}
